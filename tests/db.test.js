838f5e4f018bc0f3a95c6a81d42af83dca2a89f6
Micah Nathaniel Titus Peautagi <micahpeautagi@gmail.com>
2019-01-25 11:07:39 +1300

    Tests for db functions

--- tests/db.test.js
+++ tests/db.test.js
@@ -10,23 +10,37 @@ beforeEach(() => {
 
 afterEach(() => testEnv.cleanup(testDb))
 
-test('getUsers gets all users', () => {
-  // One for each letter of the alphabet!
-  const expected = 26
-  return db.getUsers(testDb)
-    .then(users => {
-      const actual = users.length
+test('getPhases gets all phases', () => {
+  const expected = 8
+  return db.getPhases(testDb)
+    .then(phases => {
+      const actual = phases.length
       expect(actual).toBe(expected)
     })
     .catch(err => expect(err).toBeNull())
 })
 
-test('getUser gets a single user', () => {
-  const expected = 'Ambitious Aardvark'
-  return db.getUser(99901, testDb)
-    .then(user => {
-      const actual = user.name
+test('getMoonPhase gets all activities and moon info', () => {
+  const expected = 2
+  const phaseId = 5
+  return db.getMoonPhase(phaseId, testDb)
+    .then(phaseActivties => {
+      const actual = phaseActivties.length
       expect(actual).toBe(expected)
+    })
+    .catch(err => expect(err).toBeNull())
+})
+
+test('getMoonPhase returns correct activities', () => {
+  const expected1 = 'Peaceful Protesting'
+  const expected2 = 'Stealth Attack'
+  const phaseId = 5
+  return db.getMoonPhase(phaseId, testDb)
+    .then(phaseActivities => {
+      const actual1 = phaseActivities[0].activity
+      const actual2 = phaseActivities[1].activity
+      expect(actual1).toBe(expected1)
+      expect(actual2).toBe(expected2)
     })
     .catch(err => expect(err).toBeNull())
 })
