16d08690365ec509391b929c9832e1f1a161cc60
Fetu-o-le-moana Teuila Tamapeau <fetu.tamapeau@gmail.com>
2019-01-25 14:01:03 +1300

    create header partial

--- routes/moon.js
+++ routes/moon.js
@@ -8,48 +8,45 @@ module.exports = router
 
 router.get('/', (req, res) => {
   db.getPhases()
-  .then(phases => {
-    res.render('index', {phases: phases})
-  }) 
-  .catch(err=>{
-    res.status(500).send(err.message)
-  })
+    .then(phases => {
+      res.render('index', {phases: phases})
+    })
+    .catch(err => {
+      res.status(500).send(err.message)
+    })
 })
 
-router.get('/phase/:id', (req, res) =>{
+router.get('/phase/:id', (req, res) => {
   const id = req.params.id
   db.getMoonPhase(id)
-  .then(formatData)
-  .then(phase =>{
-    res.render('phase', phase)
-  })
+    .then(formatData)
+    .then(phase => {
+      res.render('phase', phase)
+    })
 
-   function formatData (phase) {
+  function formatData (phase) {
     const data = {
       phaseId: phase[0].phaseId,
       name: phase[0].phaseName,
       image: phase[0].image,
       nextDate: phase[0].nextDate,
       activities: phase.map(el => el.activity)
-}
- return data
-   }})
+    }
+    return data
+  }
+ })
 
-router.get('/addAct', (req, res) =>{
+router.get('/addAct', (req, res) => {
   res.render('add-activity')
-  
 })
 
-router.post('/addAct' , (req, res) => {
+router.post('/addAct', (req, res) => {
   db.addActivity(req.body.title)
-  .then(res.redirect('/'))
-  .catch(err => {
-    res.status(500).send(err.message)
-  })
+    .then(res.redirect('/'))
+    .catch(err => {
+      res.status(500).send(err.message)
+    })
 })
-
-
-
 
 // Micah's try
 // router.post('/phase')
